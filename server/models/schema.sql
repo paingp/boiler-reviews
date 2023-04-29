4f76d16f0ac0d432c45de48e5ccb3383bd82bc04
Thwandall Philemon <tphilemo@purdue.edu>
2023-04-29 11:35:36 -0400

    fix unnexpected error

--- server/models/schema.sql
+++ server/models/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE Reviews (
     review_id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(review_id),
     reviewer_id INTEGER,
-    -- FOREIGN KEY (reviewer_id) REFERENCES users(user_id),
+ 
     instructor VARCHAR(24),
     term VARCHAR(6),
     years YEAR,
@@ -27,7 +27,31 @@ CREATE TABLE Reviews (
     difficulty DECIMAL(2, 1),
     standardized DECIMAL(2, 1),
     interesting DECIMAL(2, 1),
-    useful DECIMAL(2, 1)
-    likes INT
+    useful DECIMAL(2, 1),
+    likes INT,
     created_at TIMESTAMP
 );
+
+CREATE OR REPLACE VIEW GetOveralAverages AS
+    Select 
+        instructor,
+        AVG(overall) as average
+    FROM reviews
+    GROUP BY instructor;
+
+DROP FUNCTION IF EXISTS getOverallAverage;
+DELIMITER //
+CREATE FUNCTION getOverallAverage(name VARCHAR(24))
+RETURNS INT
+LANGUAGE SQL
+READS SQL DATA
+BEGIN
+    SET @result=0;
+    SELECT
+        AVG(overall) INTO @result
+    FROM reviews
+    WHERE instructor = name
+    GROUP BY instructor;
+    RETURN @result;
+END //
+DELIMITER ;
--- server/server.js
+++ server/server.js
@@ -35,14 +35,37 @@ app.post('/submit', function(req, res) {
     const review = req.body
     const started = getCurrentTime()
     const delimited = review.instr[0].split(',')
-    ServerMySQLConnection.query(`INSERT INTO reviews (review_id, reviewer_id, instructor, term, years, workload, grade, review, overall, difficulty, standardized, interesting, useful, likes, created_at)
-    VALUES (${Math.random()}, ${Math.random()}, '${delimited[0]} ${delimited[1]}', '${review.term}', ${review.year}, ${review.workload}, '${review.grade}', '${review.review}', ${review.overall}, ${review.difficulty}, ${review.standardized}, ${review.interesting}, ${review.useful}, ${0}, '${started}')`), async (err, rows, fields) => {
+    ServerMySQLConnection.query(`INSERT INTO reviews (reviewer_id, instructor, term, years, workload, grade, review, overall, difficulty, standardized, interesting, useful, likes, created_at)
+    VALUES (${Math.random()}, '${delimited[0]} ${delimited[1]}', '${review.term}', ${review.year}, ${review.workload}, '${review.grade}', '${review.review}', ${review.overall}, ${review.difficulty}, ${review.standardized}, ${review.interesting}, ${review.useful}, ${0}, '${started}')`, async (err, rows, fields) => {
         if (err) console.log(err);
         else {
             console.log('LOGGING DB TABLES;')
             console.log('ROWS', rows);
             console.log('FIELDS', fields);
         }
+    });
+});
+
+app.get('/overalls', function(req, res) {
+    
+    const { name } = req.query;
+    console.log("name = " + name);
+    if (!name) {
+        ServerMySQLConnection.query(`SELECT * FROM getoveralaverages;`, async (err, rows, fields) => {
+            if (err) console.log(err);
+            else {
+                res.send(rows);
+            }
+        });
+    } else {
+        console.log("hello");
+        ServerMySQLConnection.query(`SELECT getOverallAverage('${name}');`, async (err, rows, fields) => {
+            if (err) console.log(err);
+            else {
+                console.log(rows)
+                res.send(`${name} -> ${JSON.stringify(rows)}`);
+            }
+        });
     }
 });
 
